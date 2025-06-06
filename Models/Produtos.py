7f6b5b4b3f4b76a8bf9fab89b889d926e53b32f5
Pedro <pe.brito09@gmail.com>
2025-06-05 21:34:52 -0300

    Criação da classe Produtos

---
+++ Models/Produtos.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+class Produto:
+    def __init__(self, codigo: int, lote: int, nome: str, peso: float, dataValidade: datetime, dataFabricacao: datetime, precoCompra: float, precoVenda: float, fornecedor: str, fabricante: str, categoria: str):
+        self.codigo = codigo
+        self.lote = lote
+        self.nome = nome
+        self.peso = peso
+        self.dataValidade = datetime.strptime(dataValidade, "%Y-%m-%d").date()
+        self.dataFabricacao = datetime.strptime(dataFabricacao, "%Y-%m-%d").date()
+        self.precoCompra = precoCompra
+        self.precoVenda = precoVenda
+        self.fornecedor = fornecedor
+        self.fabricante = fabricante
+        self.categoria = categoria
+    
+    def __str__(self):
+        return f"{self.nome} (Cód: {self.codigo}, Lote: {self.lote}, Val: {self.dataValidade})"
